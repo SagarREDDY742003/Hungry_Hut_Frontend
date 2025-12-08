a59df465b8407a5927e57aab6580c75614a272b7
SagarREDDY742003 <sagar07042003@gmail.com>
2025-12-08 22:30:26 +0530

    add

--- src/component/Profile/Orders/OrderTable.jsx
+++ src/component/Profile/Orders/OrderTable.jsx
@@ -86,7 +86,7 @@ const OrderTable = ({ order }) => {
       </div>
 
       {/* Items */}
-      <div className="space-y-3 mt-3 flex flex-row flex-wrap gap-4">
+      <div className="space-y-3 mt-3 flex flex-row">
         {order.items.map((item, idx) => (
           <div
             key={item.id}
